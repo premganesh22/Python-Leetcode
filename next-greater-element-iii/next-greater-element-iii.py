3cec4ec40e53a4efd860a22e3dbebd9fb77cff24
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2021-09-21 20:25:09 -0700

    Time: 20 ms (54.33%), Space: 13.5 MB (19.69%) - LeetHub

---
+++ next-greater-element-iii/next-greater-element-iii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def nextGreaterElement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        s = list(str(n))
+        
+        if len(set(s)) == 1:
+            return -1
+        
+        index = len(s)-2
+        while s >= 0:
+            if s[index] >=s[index+1]:
+                index-=1
+            else:
+                break
+        if index < 0:
+            return -1
+        position = len(s)-1
+        while s[index] >= s[position]:
+            position-=1
+        
+        s[index],s[position] = s[position],s[index]
+        s[index+1:] = sorted(s[index+1:])
+        return int(''.join(s)) if int(''.join(s)) < 2 ** 31 else -1
