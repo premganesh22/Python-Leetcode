190f435233c95520e75cb92a145fc945aebcb5aa
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2021-09-23 16:14:44 -0700

    Time: 36 ms (55.64%), Space: 14.4 MB (12.12%) - LeetHub

--- game-of-life/game-of-life.py
+++ game-of-life/game-of-life.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+       
         def count_nei(r,c):
             nei = 0
             for i in range(r-1,r+2): #
@@ -10,26 +11,35 @@ class Solution:
                     if (r==i and c==j) or i < 0 or j < 0 or i == len(board) or j == len(board[0]):
                         continue
 
-                    elif board[i][j] == 1:
+                    elif board[i][j] in [1,3]:
                         nei+=1
             return nei
 
-        output = []
+
         for r in range(len(board)):
-            temp = []
             for c in range(len(board[0])):
                 nei = count_nei(r,c)
                 if nei < 2:
-                    temp.append(0)
+                    if board[r][c] == 1:
+                        board[r][c] = 1
+                    else:
+                        board[r][c] = 0
+
                 elif board[r][c] == 1 and nei in [2,3]:
-                    temp.append(1)
+                    board[r][c] = 3
+
                 elif board[r][c] == 0 and nei == 3:
-                    temp.append(1)
+                    board[r][c] = 2
+
                 else:
-                    temp.append(0)
-            output.append(temp)
-        
-        for i in range(len(output)):
-            for j in range(len(output[0])):
-                board[i][j] = output[i][j]
-        #return board
+                    if board[r][c] == 1:
+                        board[r][c] = 1
+                    else:
+                        board[r][c] = 0
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == 1:
+                    board[r][c] = 0
+                elif board[r][c] in [2,3]:
+                    board[r][c] = 1
