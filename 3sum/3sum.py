610740c0ef3f45a1870630d41a7c7197d9f621a8
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2021-10-03 00:03:00 -0700

    Time: 680 ms (93.63%), Space: 17.8 MB (34.35%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,57 +1,34 @@
+
+
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        output = []
         nums.sort()
-        for i in range(len(nums)):
+        target = 0
+        for i in range(len(nums)-2):
+            left = i+1
+            right = len(nums)-1
+
             if nums[i] > 0:
-                break
-            if i == 0 or nums[i - 1] != nums[i]:
-                self.twoSumII(nums, i, res)
-        return res
+                return output
 
-    def twoSumII(self, nums: List[int], i: int, res: List[List[int]]):
-        lo, hi = i + 1, len(nums) - 1
-        while (lo < hi):
-            sum = nums[i] + nums[lo] + nums[hi]
-            if sum < 0:
-                lo += 1
-            elif sum > 0:
-                hi -= 1
-            else:
-                res.append([nums[i], nums[lo], nums[hi]])
-                lo += 1
-                hi -= 1
-                while lo < hi and nums[lo] == nums[lo - 1]:
-                    lo += 1
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
 
-
-# def two_sum(nums,i):
-#     found = []
-#     left=i+1
-#     right=len(nums)-1
-#     while left < right:
-#         if nums[i] + nums[left] + nums[right] == 0:
-#             found.append([nums[i], nums[left], nums[right]])
-#             right-=1
-#             left+=1
-#             while left<right and nums[left] == nums[left-1]:
-#                 left+=1
-#         elif nums[i] + nums[left]+nums[right] > 0:
-#             right-=1
-#         else:
-#             left+=1
-#     return found
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         output = []
-#         nums.sort()
-#         for i in range(len(nums)-2):
-#             if nums[i] > 0:
-#                 break
-#             if nums[i] == 0 or nums[i-1] != nums[i]:
-#                 found = two_sum(nums,i)
+            while left < right:
+                val = nums[i] + nums[left] + nums[right]
+                if val > target:
+                    right-=1
+                elif val < target:
+                    left+=1
+                else:
+                    output.append([nums[i], nums[left], nums[right]])
         
-#                 if found:
-#                     output.extend(found)
-#         return output
-        
+                    left+=1
+                    right-=1
+
+                    while left < right and nums[left] == nums[left-1]:
+                        print(left)
+                        left+=1
+        return output
