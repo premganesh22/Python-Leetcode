9b1d287943c9d0fd03df94699774945a15352759
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2021-10-03 01:49:39 -0700

    Time: 860 ms (73.97%), Space: 17.5 MB (49.31%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -6,29 +6,28 @@ class Solution:
         output = []
         nums.sort()
         target = 0
-        for i in range(len(nums)-2):
+        print(nums)
+        i=0
+        while i < len(nums)-2:
             left = i+1
             right = len(nums)-1
-
-            if nums[i] > 0:
-                return output
-
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-
-            while left < right:
+            while left<right:
                 val = nums[i] + nums[left] + nums[right]
                 if val > target:
                     right-=1
                 elif val < target:
                     left+=1
                 else:
-                    output.append([nums[i], nums[left], nums[right]])
-        
+                    output.append([nums[i],nums[left],nums[right]])
                     left+=1
                     right-=1
-
                     while left < right and nums[left] == nums[left-1]:
-                        print(left)
                         left+=1
+                    while left < right and nums[right] == nums[right+1]:
+                        right-=1
+
+            i+=1
+            while i < len(nums)-2 and nums[i] == nums[i-1]:
+                i+=1
+
         return output
