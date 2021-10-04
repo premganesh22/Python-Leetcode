c4cba113e976608c8c89186c551357a2c787dca5
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2021-10-03 23:06:40 -0700

    Time: 40 ms (89.36%), Space: 14.5 MB (21.86%) - LeetHub

---
+++ count-and-say/count-and-say.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return '1'
+        prev = self.countAndSay(n-1)
+        result = ''
+        ct = 1
+        for i in range(len(prev)):
+            if i == len(prev)-1 or prev[i] != prev[i+1]:
+                result+=str(ct)+prev[i]
+                ct = 1
+            else:
+                ct+=1
+        return result
+        
