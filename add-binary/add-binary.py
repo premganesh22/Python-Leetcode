d2f78bfc5ad68183fb029ff57602f983b906735a
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2021-10-04 18:23:17 -0700

    Time: 63 ms (6.82%), Space: 14.4 MB (21.47%) - LeetHub

---
+++ add-binary/add-binary.py
@@ -0,0 +1,19 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        count = []
+        carry = 0
+        i = 0
+        a = a[::-1]
+        b = b[::-1]
+        print(a,b)
+        while i < len(a) or i < len(b) or carry:
+            if i<len(a):
+                carry+=int(a[i])
+            if i<len(b):
+                carry+=int(b[i])
+            count.append(str(carry%2))
+            carry = carry//2
+            i+=1
+
+        return ''.join(count[::-1])
+        
