0230637aa0422fb84c15847a94e968f659d24ddc
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2021-11-21 23:45:14 -0800

    Time: 370 ms (54.17%), Space: 16.9 MB (48.18%) - LeetHub

--- fair-candy-swap/fair-candy-swap.py
+++ fair-candy-swap/fair-candy-swap.py
@@ -1,10 +1,12 @@
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
-        dic = {}
-        for i in aliceSizes:
-            dic[i] = 1
 
-        difference = int((sum(aliceSizes) - sum(bobSizes)) / 2)
-        for i in bobSizes:
-            if difference+i in dic:
-                return [difference+i,i]
+        dic = defaultdict(int)
+        for num in aliceSizes:
+            dic[num] = 0
+        diff = (sum(aliceSizes) - sum(bobSizes)) //2
+        for num in bobSizes:
+            if num+diff in dic:
+                return [num+diff,num]
+            
+        
