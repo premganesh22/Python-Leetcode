7a16a6c37f03507684cb66411053fc193ca138c4
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-03-09 21:30:09 -0800

    Time: 795 ms (5.04%), Space: 14 MB (73.26%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,28 +1,29 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         def helper(candidates,target,slate,result):
-            total = sum(slate)
-            if total == target:
+            
+            if sum(slate) == target:
                 result.append(slate[:])
+                return 
+            if sum(slate) > target:
                 return
-            elif total > target or len(candidates) == 0:
+            
+            if len(candidates) == 0:
                 return
-            else:
-                #Include
-                count = 0
-                cur_num = candidates[0]
-                for i in range(len(candidates)):
-                    if cur_num == candidates[i]:
-                        count+=1
-                for i in range(count):
-                    slate.append(candidates[i])
-                    helper(candidates[count:],target,slate,result)
-                for i in range(count):
-                    slate.pop()
-                    
-                #Exclude
+            cur_num = candidates[0]
+            count=0
+            for i in range(len(candidates)):
+                if candidates[i] == cur_num:
+                    count+=1
+            #Include
+            for i in range(count):
+                slate.append(candidates[i])
                 helper(candidates[count:],target,slate,result)
-                return result
-        
+            
+            for i in range(count):
+                slate.pop()
+            
+            #Exclude
+            helper(candidates[count:],target,slate,result)
+            return result
         return helper(sorted(candidates),target,[],[])
-        
