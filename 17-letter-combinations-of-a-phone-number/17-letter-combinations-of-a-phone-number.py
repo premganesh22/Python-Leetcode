bb108058898a7597d057bb45e4fb294b6ce8c2a9
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-03-09 22:24:06 -0800

    Time: 60 ms (13.14%), Space: 13.9 MB (88.31%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -3,18 +3,17 @@ class Solution:
         dic = { "2": "abc", "3": "def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
 
         def phone(digits,digit_idx,slate,result):
-            #Base case
             if digit_idx == len(digits):
                 result.append(''.join(slate))
                 return
-            
-            #Recrusive
             else:
                 string = dic[digits[digit_idx]]
-                for s in string:
-                    slate.append(s)
+                for i in range(len(string)):
+                    slate.append(string[i])
                     phone(digits,digit_idx+1,slate,result)
                     slate.pop()
                 return result
         return phone(digits,0,[],[])
-        
+            
+                    
+                    
