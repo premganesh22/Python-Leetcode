2b07b67495672c2516268ba986a9d1780edea4c5
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-03-09 22:45:15 -0800

    Time: 664 ms (94.52%), Space: 29 MB (73.65%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,26 +1,17 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        def is_pali(s):
+            return s == s[::-1]
         
-        def is_pali(s,idx,i):
-            return s[idx:i+1] == s[idx:i+1][::-1]
-
-        def pali(s,idx,slate,result):
+        def helper(s,idx,stack,result):
             if idx == len(s):
-                temp = []
-                x = 0
-                for index in slate:
-                    temp.append(s[x:index+1])
-                    x = index+1
-                result.append(temp[:])
-                return
+                result.append(stack[:])
+                
             else:
                 for i in range(idx,len(s)):
-                    if is_pali(s,idx,i):
-                        slate.append(i)
-                        pali(s,i+1,slate,result)
-                        slate.pop()
+                    if is_pali(s[idx:i+1]):
+                        stack.append(s[idx:i+1])
+                        helper(s,i+1,stack,result)
+                        stack.pop()
                 return result
-
-
-        return pali(s,0,[],[]) 
-        
+        return helper(s,0,[],[])
