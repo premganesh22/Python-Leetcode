38bd2971c2372876045f05cdeaab6b480a507ff6
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-03-09 19:18:24 -0800

    Time: 45 ms (73.79%), Space: 14 MB (90.60%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,16 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        def helper(nums,slate,result):
+        def helper(nums,idx,slate,result):
             
-            if len(nums) == 0:
+            if len(nums) == idx:
                 result.append(slate[:])
                 return
             else:
-                for i in range(len(nums)):
-                    slate.append(nums[i])
-                    helper(nums[:i]+nums[i+1:],slate,result)
+                for i in range(idx,len(nums)):
+                    nums[idx],nums[i] = nums[i],nums[idx]
+                    slate.append(nums[idx])
+                    helper(nums,idx+1,slate,result)
+                    nums[idx],nums[i] = nums[i],nums[idx]
                     slate.pop()
                 return result
-        return helper(nums,[],[])
+        return helper(nums,0,[],[])
         
