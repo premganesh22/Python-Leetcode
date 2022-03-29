0db9db21f04276bb4bd18597583e7b06433f2e83
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-03-29 16:46:57 -0700

    Time: 51 ms (63.45%), Space: 14 MB (47.33%) - LeetHub

---
+++ 139-word-break/139-word-break.py
@@ -0,0 +1,26 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        #Recursion
+#         word_list = list(s)
+#         dic_set  = set(wordDict)
+        
+#         def recursion(index):
+#             if index == len(word_list):
+#                 return True
+#             for i in range(0,len(word_list)-index):
+#                 if ''.join(word_list[index:(index+i+1)]) in dic_set:
+#                     if recursion(index+i+1):
+#                         return True
+#             return False
+#         return recursion(0)
+        
+        #DP
+        table = [False]*(len(s)+1)
+        table[0] = True
+        wordDict = set(wordDict)
+        for i in range(1,len(s)+1):
+            for wordlength in range(1,i+1):
+                if s[i-wordlength:i] in wordDict and table[i-wordlength]:
+                    table[i] = True
+        return table[len(s)]
