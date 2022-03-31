1593da1fbafe57403f58dcddfc0ad57a6835f35a
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-03-31 08:10:52 -0700

    Time: 2499 ms (25.71%), Space: 14.3 MB (46.55%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,5 +1,6 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        
         output = [float('inf')]*(amount+1)
         output[0] = 0
         
@@ -13,3 +14,18 @@ class Solution:
         if output[amount] == float('inf'):
             return -1
         return output[amount]
+
+#         Recusion
+#         global_count = [float('inf')]
+#         def change(money,step):
+#             #print(money,step)
+#             if money<0:
+#                 return
+#             if money == 0:
+#                 global_count[0] = min(step,global_count[0])
+#                 return
+#             for coin in coins:
+#                 change(money-coin,step+1)
+                
+#         change(amount,0)
+#         return global_count[0] if global_count[0] < float('inf') else -1
