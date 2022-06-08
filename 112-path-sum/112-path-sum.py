ba9c9976ac06b625b1f8185fe660d6f59bfbad9d
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-06-08 15:08:57 -0700

    Time: 90 ms (6.72%), Space: 15.1 MB (57.48%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -6,25 +6,21 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        global_box = [False]
         if not root:
-            return False
+            return None
+        result = [False]
         
-        def dfs(root,targetSum):
-            #Constraint
-            if global_box[0] == True:
-                return
-            #Base Case
-            if not root.left and not root.right:
-                if targetSum == root.val:
-                    global_box[0] = True
-                return 
-            #Recursive Logic
-            if root.left:
-                dfs(root.left,targetSum-root.val)
-            if root.right:
-                dfs(root.right,targetSum-root.val)
-            return 
+        def helper(node, targetSum):
+            if not node.left and not node.right:
+                if targetSum == 0:
+                    result[0] = True
+            
+            if node.left:
+                helper(node.left, targetSum-node.left.val)
+            
+            if node.right:
+                helper(node.right, targetSum-node.right.val)
         
-        dfs(root,targetSum)
-        return global_box[0]
+        helper(root, targetSum-root.val)
+            
+        return result[0]
