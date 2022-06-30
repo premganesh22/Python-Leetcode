c6e47a6edb5bb8f02cfb19404bb467407138ee0c
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-06-29 21:41:39 -0700

    Time: 485 ms (30.61%), Space: 16.5 MB (73.15%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,20 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix_map = {}
-        prefix_map[0] = 1
-        prefix_sum = 0
-        output = 0
-        for num in nums:
-            prefix_sum+=num
-           
-            if prefix_sum-k in prefix_map:
-                output+= prefix_map[prefix_sum-k]
+        prefix = 0
+        hmap = {0:1}
+        global_output = 0
+        for i in range(len(nums)):
+            prefix+= nums[i]
+            if prefix-k in hmap:
+                global_output+= hmap[prefix-k]
             
-            if prefix_sum in prefix_map:
-                prefix_map[prefix_sum]+=1
-            
+            if prefix in hmap:
+                hmap[prefix]+=1
             else:
-                prefix_map[prefix_sum] = 1
-        
-        return output
-            
+                hmap[prefix] = 1
+        return global_output
