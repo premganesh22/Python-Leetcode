427c2dc6f3a14de399e1c55351679d374f0b0826
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-07-07 13:47:12 -0700

    Time: 670 ms (12.70%), Space: 16.5 MB (48.17%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,35 +1,41 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def get_neighbors(r,c):
-            neighbors = [[-1,0],[1,0],[0,-1],[0,1]]
-            result = []
-            for nr,nc in neighbors:
-                row = nr+r
-                col = nc+c
-                if row in range(0,len(grid)) and col in range(0,len(grid[0])):
-                    result.append((row,col))
-            return result
-
-        def BFS(r,c):
-            queue = deque()
-            queue.append((r,c))
-            grid[r][c] = '0'
-            while queue:
-                r,c = queue.popleft()
-                for row,col in get_neighbors(r,c):
-                    if grid[row][col] == '1':
-                        queue.append((row,col))
-                        grid[row][col] = '0'
-
-
-
-        island = 0
-        row = len(grid)
-        col = len(grid[0])
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == '1':
-                    island+=1
-                    BFS(r,c)
-        return island
         
+        def get_neighbours(m, n):
+            output = []
+            for x, y in [(0,1), (0,-1), (1,0), (-1,0)]:
+                if 0 <= m+x < len(grid) and 0 <= n+y < len(grid[0]):
+                    output.append((m+x, n+y))
+            
+            return output
+        
+        def BFS(node):
+            
+            queue = deque()
+            queue.append(node)
+            
+            while queue:
+                m, n = queue.popleft()
+                grid[m][n] = "0"
+                neighbours = get_neighbours(m, n)
+                for x, y in neighbours:
+                    if grid[x][y] == '1':
+                        queue.append((x,y))
+        
+        def DFS(node):
+            m, n = node[0], node[1]
+            grid[m][n] = "0"
+            neighbours = get_neighbours(m, n)
+            for x, y in neighbours:
+                if grid[x][y] == '1':
+                    DFS((x,y))
+            
+        
+        island= 0
+        for m in range(len(grid)):
+            for n in range(len(grid[0])):
+                if grid[m][n] == '1':
+                    island+=1
+                    DFS((m,n))
+        
+        return island
