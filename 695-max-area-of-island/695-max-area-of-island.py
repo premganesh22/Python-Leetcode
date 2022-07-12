6a771b5005497e1616ea54e7422474ac34cf3fe1
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-07-11 23:32:17 -0700

    Time: 354 ms (5.63%), Space: 18.3 MB (5.12%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,40 +1,28 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        def get_neighbors(r,c):
-            neighbors = [[-1,0],[1,0],[0,-1],[0,1]]
-            result = []
-            for nr,nc in neighbors:
-                row = nr+r
-                col = nc+c
-                if row in range(0,len(grid)) and col in range(0,len(grid[0])):
-                    result.append((row,col))
-            return result
-
-        def BFS(r,c):
-            land = 1
-            queue = deque()
-            queue.append((r,c))
-            grid[r][c] = 0
-            while queue:
-                r,c = queue.popleft()
-                #print(get_neighbors(r,c))
-                for row,col in get_neighbors(r,c):
-                    if grid[row][col] == 1:
-                        land+=1
-                        queue.append((row,col))
-                        grid[row][col] = 0
-            return land
-
-
-        island = 0
-        row = len(grid)
-        col = len(grid[0])
-        max_land = 0
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == 1:
-                    island+=1
-                    land = BFS(r,c)
-                    max_land = max(max_land,land)
-        return max_land
         
+        def get_neigbhours(node):
+            direction = [[0,1], [0,-1], [1, 0], [-1,0]]
+            output = []
+            for x,y in direction:
+                if 0 <= node[0]+x < len(grid) and 0<= node[1]+y < len(grid[0]) and grid[node[0]+x][node[1]+y] == 1:
+                    output.append((node[0]+x, node[1]+y))
+            return output
+        
+        def dfs(node):
+            visited.add(node)
+            global_count[0]+=1
+            for nei in get_neigbhours(node):
+                if nei not in visited:
+                    dfs(nei)
+                    
+        global_count = [0]
+        max_island = 0
+        visited = set()
+        for m in range(len(grid)):
+            for n in range(len(grid[0])):
+                if (m,n) not in visited and grid[m][n] == 1:
+                    global_count = [0]
+                    dfs((m,n))
+                    max_island = max(max_island, global_count[0])
+        return max_island
