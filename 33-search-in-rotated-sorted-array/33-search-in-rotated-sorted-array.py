17e6e567a90b44840efb16bb598e39cac6a1872d
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-07-18 21:39:24 -0700

    Time: 65 ms (48.92%), Space: 14.4 MB (18.93%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,24 +1,31 @@
 class Solution:
-    def search(self, lista: List[int], key: int) -> int:
-            start = 0
-            end = len(lista)-1
-            while start <= end:
-
-                mid = start + (end-start)//2
-                if lista[mid] == key:
-                    return mid
-
-                #Left Portion
-                if lista[start] <= lista[mid]:
-                    if lista[mid] < key or lista[start] > key:
-                        start = mid+1
-                    else:
-                        end = mid-1
-
-                #Right Portion
-                else:
-                    if lista[mid] > key or lista[end] < key:
-                        end = mid-1
-                    else:
-                        start = mid+1
-            return -1
+    def search(self, nums: List[int], target: int) -> int:
+        
+        start = 0
+        end = len(nums)-1
+        
+        zone = 'white' if nums[0] < nums[-1] else ('green' if target <= nums[-1] else 'brown')
+        
+        while start <= end:
+            mid = start + (end-start)//2
+            
+            if target == nums[mid]:
+                return mid
+            
+            #Being in Wrong zone
+            if zone == 'green' and nums[-1] <= nums[mid]:
+                start = mid+1
+                
+            elif zone == 'brown' and nums[-1] > nums[mid]:
+                end = mid-1
+                
+            
+            #Normal binary search
+            
+            elif nums[mid] < target:
+                start+=1
+            else:
+                end-=1
+                
+        return -1
+                
