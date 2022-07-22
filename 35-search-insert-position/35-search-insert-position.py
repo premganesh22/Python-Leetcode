fa801c69ae9f57e189436b4d284ce42e2af2e744
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-07-22 15:47:09 -0700

    Time: 54 ms (89.73%), Space: 14.6 MB (82.82%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -6,13 +6,9 @@ class Solution:
         while start <= end:
             mid = start + (end-start)//2
             
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 start = mid+1
-            
+                
             else:
-                end= mid-1
-        
+                end = mid-1
         return start
-        
