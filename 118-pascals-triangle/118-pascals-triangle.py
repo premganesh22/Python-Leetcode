e65e5ef9c95b1b0b150933d4adc3b0b911f639ad
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-07-23 11:15:55 -0700

    Time: 54 ms (32.11%), Space: 14 MB (17.88%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,7 +1,11 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        
         table = [[1]*i for i in range(1,numRows+1)]
-        for rows in range(2,numRows):
-            for col in range(1,len(table[rows])-1):
-                table[rows][col] = table[rows-1][col] + table[rows-1][col-1]
+        
+        for i in range(2,len(table)):
+            for j in range(1,len(table[i])-1):
+                table[i][j] = table[i-1][j-1] + table[i-1][j]
+        
         return table
+        
