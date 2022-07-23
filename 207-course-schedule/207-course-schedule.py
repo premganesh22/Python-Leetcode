c478dff6f0c406e765322771f79e286bf9d03284
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-07-22 21:26:15 -0700

    Time: 194 ms (23.25%), Space: 17.4 MB (12.35%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,51 +1,38 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj_list = [[] for i in range(numCourses)]
-        for cor, pre in prerequisites:
-            adj_list[pre].append(cor)
+        
+        #Build Adjacency list
+        a_map = {}
+        for c, d in prerequisites:
+            if d in a_map:
+                a_map[d].append(c)
+            else:
+                a_map[d] = [c]
+        
+        
+        arrival = set()
+        visited = set()
+        departure = set()
+        global_status = [True]
+        def dfs(node):
+            
+            arrival.add(node)
+            visited.add(node)
+            if node in a_map:
+                for course in a_map[node]:
+                    if course not in visited:
+                        dfs(course)
 
-        visited = [-1]*numCourses
-        arrival = [-1]*numCourses
-        departure = [-1]*numCourses
+                    else:
+                        if course not in departure:
+                            global_status[0] = False
 
-        timestamp = [0]
-
-#         def dfs(node):
-#             visited[node] = 1
-#             arrival[node] = global_time[0]
-#             global_time[0]+=1
-                
-#             for nei in adj_list[node]:
-#                 if visited[nei] == -1:
-#                     if not dfs(nei):
-#                         return False
-#                 else:
-#                     if departure[node] == -1:
-#                         return False
-
-#             departure[node] = global_time[0]
-#             global_time[0]+=1
-#             return True
-
-        def dfs(s):
-
-            arrival[s] = timestamp[0]
-            timestamp[0]+=1
-            visited[s] = 1
-            for edge in adj_list[s]:
-                if visited[edge] == -1:
-                    if not dfs(edge):
-                        return False
-                else:
-                    if departure[edge] == -1:
-                        return False
-            departure[s] = timestamp[0]
-            timestamp[0]+=1
-            return True
-
-
-        for node in range(numCourses):
-            if visited[node] == -1:
-                if not dfs(node):
-                    return False
-        return True
+            departure.add(node)
+            
+        for node in a_map:
+            if node not in visited:
+                dfs(node)
+        return global_status[0]
+                    
+        
+        
