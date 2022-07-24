c0876f3f5a2ca58331ab4c7e55b48b67888c950c
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-07-24 11:09:22 -0700

    Time: 48 ms (62.17%), Space: 14 MB (96.28%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,17 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def sub(s,slate,result):
-            if len(s) == 0:
-                result.append(slate.copy())
-                return result
-            else:
-                #Include
-                slate.append(s[0])
-                sub(s[1:],slate,result)
-                slate.pop()
-
-                #Exlcude
-                sub(s[1:],slate,result)
-                return result
-        return sub(nums,[],[])
         
+        output = []
+        def helper(nums, slate):
+            if len(nums) == 0:
+                output.append(slate[:])
+                return
+
+            #Include
+            slate.append(nums[0])
+            helper(nums[1:], slate)
+            slate.pop()
+
+            #Exclude
+            helper(nums[1:], slate)
+                
+                
+        
+        helper(nums, [])
+        return output
