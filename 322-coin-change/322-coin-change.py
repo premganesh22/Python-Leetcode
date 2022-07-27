40f5a3cc39e77194c26e1eed357144fdd2ea6d15
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-07-27 11:22:10 -0700

    Time: 3195 ms (13.24%), Space: 14.2 MB (55.54%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,31 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        output = [float('inf')]*(amount+1)
-        output[0] = 0
-        
-        for val in range(1,amount+1):
-            find_min = float('inf')
-            for coin in coins:
-                if val-coin>=0:
-                    find_min = min(output[val-coin],find_min)
-                    
-            output[val] = find_min+1
-        if output[amount] == float('inf'):
-            return -1
-        return output[amount]
+     
 
-#         Recusion
-#         global_count = [float('inf')]
-#         def change(money,step):
-#             #print(money,step)
-#             if money<0:
-#                 return
-#             if money == 0:
-#                 global_count[0] = min(step,global_count[0])
-#                 return
-#             for coin in coins:
-#                 change(money-coin,step+1)
-                
-#         change(amount,0)
-#         return global_count[0] if global_count[0] < float('inf') else -1
+        table = [-1]*(amount+1)
+        table[0] = 0
+        for i in range(1, len(table[1:])+1):
+            min_val = float('inf')
+            for coin in coins:
+                if i-coin >= 0 and table[i-coin] != -1:
+                    min_val = min(min_val,table[i-coin])
+            table[i] = min_val+1 if min_val != float('inf') else -1
+        return table[amount]
+        
