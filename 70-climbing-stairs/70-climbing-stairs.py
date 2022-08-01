4a994de5ecfd845b34180fc50dbfbef7996104fc
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-08-01 13:37:58 -0700

    Time: 40 ms (70.01%), Space: 13.8 MB (96.27%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,11 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n ==1:
-            return 1
-        table = [0]*n
-        table[0] = 1
-        table[1] = 2
-        for i in range(2,n):
-            table[i] = table[i-1] + table[i-2]
-        return table[n-1]
+        
+        
+        result = [-1]*(n+1)
+        
+        result[0] = 1
+        result[1] = 1
+        
+        if n <2:
+            return result[n]
+        
+        for i in range(2,n+1):
+            result[i] = result[i-1] + result[i-2]
+            
+        return result[-1]
         
