99dc410527d5f3280a19121780efc9d31d130b1a
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-08-16 20:27:11 -0700

    Time: 208 ms (62.12%), Space: 16.3 MB (25.96%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -3,42 +3,38 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        direction = [[0,1], [0,-1], [1, 0], [-1, 0]]
-        def get_nei(x,y):
+        
+        def get_adjacent(row,col):
             output = []
-            for m, n in direction:
-                if 0 <= x+m < len(board) and 0 <= y+n < len(board[0]):
-                    output.append([x+m, y+n])
+            for x, y in [[1,0], [-1,0], [0,1], [0,-1]]:
+                if 0 <= row+x < len(board) and 0 <= col+y < len(board[0]) and board[row+x][col+y] == 'O':
+                    output.append([row+x, col+y])
             return output
         
-        def dfs(x,y):
-            board[x][y] = 'T'
-            for m,n in get_nei(x,y):
-                if board[m][n] == 'O':
-                    dfs(m,n)
-                
-        
-        
-        for x in [0, len(board)-1]:
-            for y in range(len(board[0])):
-                if board[x][y] == 'O':
-                    dfs(x, y)
-                
-        for x in range(1,len(board)):
-            for y in [0, len(board[0])-1]:
-                print(x,y)
-                if board[x][y] == 'O':
-                    dfs(x, y)
-        
-        for x in range(len(board)):
-            for y in range(len(board[0])):
-                if board[x][y] == 'T':
-                    board[x][y] = 'O'
-                elif board[x][y] == 'O':
-                    board[x][y] = 'X'
+        def dfs(row,col):
+            visited.add((row,col))
+            board[row][col] = '-1'
+            for x, y in get_adjacent(row,col):
+                if not (x,y) in visited:
+                    dfs(x,y)
                     
+        visited = set()
         
-                    
+        for row in [0, len(board)-1]:
+            for col in range(len(board[0])):
+                if board[row][col] == 'O' and (row,col) not in visited:
+                            
+                    dfs(row,col)
         
+        for row in range(len(board)):
+            for col in [0, len(board[0])-1]:
+                    if board[row][col] == 'O' and (row,col) not in visited:
+                            
+                        dfs(row,col)
         
-        
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == '-1':
+                    board[row][col] = 'O'
+                elif board[row][col] == 'O':
+                    board[row][col] = 'X'        
