79532416b8a525b316a20d78338b802db3f315fe
premganesh22 <43224209+premganesh22@users.noreply.github.com>
2022-08-24 21:48:59 -0700

    Time: 1580 ms (40.96%), Space: 17.1 MB (69.51%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,25 +1,5 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-#         a_list = {}
-#         supplies = set(supplies)
-#         for item in range(len(receipes)):
-#             a_list[receipes[item]] = ingredients[item]
-            
-#         output = []
-#         def bfs(item):
-#             if item in a_list:
-#                 for nei in a_list[item]:
-#                     bfs(nei)
-                    
-#             if 
-            
-            
-            
-#         for receipe in a_list:
-#             dfs(receipe)
-
-
-        #receipe to Ingrediant length
     
         reci = {}
         for r in range(len(recipes)):
@@ -39,7 +19,7 @@ class Solution:
         for item in supplies:
             print(item)
             q.append(item)
-        print(q)
+            
         ans = []
         while q:
             ingredient = q.popleft()
